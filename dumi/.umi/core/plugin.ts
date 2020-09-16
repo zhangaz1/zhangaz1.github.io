8a3d7f45dbb3c4a3b616a8a817d4587124213c9c
zhangaz1 <zhangaz1@hotmail.com>
2020-09-16 19:26:11 +0800

    update dumi with doc:build

--- dumi/.umi/core/plugin.ts
+++
@@ -1,8 +0,0 @@
-// @ts-nocheck
-import { Plugin } from '/Users/zhangaz1/workspace/learning/umijs/dumi/node_modules/@umijs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
-});
-
-export { plugin };
