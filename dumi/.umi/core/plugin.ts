e6b93187e5743eb4f1a1b313280f0e5f0fd028b9
zhangaz1 <zhangaz1@hotmail.com>
2020-09-16 13:28:42 +0800

    add dumi for test

---
+++ dumi/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/zhangaz1/workspace/learning/umijs/dumi/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange',],
+});
+
+export { plugin };
